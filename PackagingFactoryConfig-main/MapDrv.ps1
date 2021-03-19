86bd95234956dc02f1d27e261d29559d23bfa6f8
satsuk81 <76114734+satsuk81@users.noreply.github.com>
2021-03-19 12:58:47 +0000

    Delete MapDrv.ps1

--- PackagingFactoryConfig-main/MapDrv.ps1
+++
@@ -1,2 +0,0 @@
-cmd.exe /C cmdkey /add:`"wlprodeusprodpkgstr01.file.core.windows.net`" /user:`"Azure\wlprodeusprodpkgstr01`" /pass:`"rg-wl-prod-packaging`"
-New-PSDrive -Name Z -PSProvider FileSystem -Root "\\wlprodeusprodpkgstr01.file.core.windows.net\pkgazfiles01" -Persist
